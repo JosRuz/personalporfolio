cedf05d199b390cf5a8fb2d6d118802fae36b04a
Josue Ruiz <josu24.42@gmail.com>
2024-05-17 21:52:42 -0600

    Initial commit

---
+++ test/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("mousemove", function(event) {
+    const text1 = document.getElementById('text1');
+    const text2 = document.getElementById('text2');
+  
+    const bodyWidth = document.body.offsetWidth;
+    const mouseX = event.clientX;
+  
+    const offset = 50; // Adjust this value for the parallax effect
+  
+    const text1Offset = (mouseX / bodyWidth * offset) - (offset / 2);
+    const text2Offset = (mouseX / bodyWidth * -offset) + (offset / 2);
+  
+    text1.style.transform = `translateX(${text1Offset}%)`;
+    text2.style.transform = `translateX(${text2Offset}%)`;
+  });
+  
